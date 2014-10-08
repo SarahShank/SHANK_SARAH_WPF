1acc92a2cdd5fcaf0df5d4bc9d604d674a6fc7c0
Sarah Shank <Sarahshank07@gmail.com>
2014-10-08 15:16:55 -0400

    Javascript Assignment
    Javascript Assignment , Practicing JS for week 2 WPF
    10-8-2014

--- assignment [values_variables]/JS/script4.js
+++ assignment [values_variables]/JS/script4.js
@@ -1,10 +1,14 @@
-/**
- * Created by SarahShank on 10/8/14.
- */
-//Arrays
+// Arrays
 
-var theGriffins = ["Peter", "Stewie", "Brian","Lois", "Meg"]; //Index starts with 0(Peter),1,2,3,4
+var theGriffins = ["Peter", "Stewie", "Brian", "Lois", "Meg"]; //Index starts with 0(Peter),1,2,3,4
 
-console.log(theGriffins);
+console.log(theGriffins[0]); //[0] = List array value
 
+theGriffins[1] = "Meg"; //change index #1 to Meg
+theGriffins[2] = "Lois";
+
+var num = 2;
+
+console.log(theGriffins[num]); //lets me see whats in the index #1 etc.
+                             // or [num]
 alert("Giggity");
