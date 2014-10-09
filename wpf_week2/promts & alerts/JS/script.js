7c058d133ad6acef81770b7297af964c7bbb3165
Sarah Shank <Sarahshank07@gmail.com>
2014-10-09 10:11:31 -0400

    Prompts & Alerts
    JS practice with prompts & alerts

---
+++ wpf_week2/promts & alerts/JS/script.js
@@ -0,0 +1,15 @@
+/**
+ * Created by SarahShank on 10/9/14.
+ */
+
+//var userInput = prompt("whats your favorite color");
+//console.log(userInput);
+
+var width = prompt ("We are calculating the area of a rectangle. \nPlease enter the width");
+var height = prompt ("Please enter the height");
+var area = width * height;//calculates with that information
+
+//console.log(area);//prints to the console
+
+var result = "The area of your rectangle is "  +  area  +  " sq feet";
+alert(result);
