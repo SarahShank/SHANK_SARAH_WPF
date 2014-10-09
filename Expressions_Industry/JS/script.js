5a48ee30681ee017a3bbd99889d725bcbe39bde4
Sarah Shank <Sarahshank07@gmail.com>
2014-10-09 18:04:15 -0400

    Expressions_wacky
    JS Expressions_wacky, homework

--- Expressions_Industry/JS/script.js
+++ Expressions_Industry/JS/script.js
@@ -5,5 +5,21 @@
  10-8-2014
  */
 
-//How long will it take to download a file?
-var fileSize = prompt("Please enter your file size")
+//Downloading files
+var fileSize = prompt("Please enter your file size");
+var internetSpeed = prompt("Please enter your internet speed");
+var
+
+var size1 = "File Size ";
+var size2 = "160 bytes";
+
+var result = size1 + size2;
+console.log(result);
+
+var  speed1 = "Internet Speed "
+var  speed2 = "10 MB"
+
+var result = speed1 + speed2;
+console.log(result);
+
+var result =
---
+++ Expressions_Wacky/JS/script.js
@@ -0,0 +1,7 @@
+/*
+ Sarah Shank
+ WPF Section 01
+ Expressions_Assignment_Personal Week 2
+ 10-8-2014
+ */
+
