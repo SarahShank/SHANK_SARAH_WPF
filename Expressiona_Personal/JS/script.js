09794c53653f5901f4587afa16b94fee4d720a6c
Sarah Shank <Sarahshank07@gmail.com>
2014-10-08 23:19:16 -0400

    Expressions_Personal
    Expressions_Personal, Assignment commits in JS

---
+++ Expressiona_Personal/JS/script.js
@@ -0,0 +1,8 @@
+/*
+Sarah Shank
+WPF Section 01
+Expressions_Assignment_Personal Week 2
+10-8-2014
+*/
+
+alert("Test");
---
+++ expressions_worksheet/JS/week2.js
@@ -0,0 +1,8 @@
+/*
+Sarah Shank
+WPF WDD144-O Section 01
+Expressions_Worksheet Week 2
+10-08-2014
+ */
+
+alert("Testing 1,2,3");
