d091d8258e80ed78fc7a7e0bf91f36a5636fef0f
Sarah Shank <Sarahshank07@gmail.com>
2014-10-09 16:58:06 -0400

    expressions personal
    JS expressions personal assignmnet

--- Expressiona_Personal/JS/script.js
+++ Expressiona_Personal/JS/script.js
@@ -12,7 +12,9 @@ var roomWidth = prompt ("Please enter the width of your room in ft.");
 //Asking the user for their room length. (Example = 12)
 var roomLength = prompt ("Please enter the length of your room ft.");
 //Asking the user for the size of your tile in sq. ft. (Example = 1.77)
-var tileSize = prompt ("Please enter the size of the tile in sq. ft.");
+//Giving the user tile options already in the form of square feet
+alert("Tile choices listed in square feet: 1, 1.77, 2.25");
+var tileSize = prompt ("Please enter the size of the tile in square feet");
 //Calculate the information.
 var tilesNeeded = roomWidth * roomLength / tileSize;
 //console.log (tilesNeeded); prints to the console.
--- Expressions_Industry/JS/script.js
+++ Expressions_Industry/JS/script.js
@@ -1,3 +1,4 @@
 /**
  * Created by SarahShank on 10/9/14.
  */
+
