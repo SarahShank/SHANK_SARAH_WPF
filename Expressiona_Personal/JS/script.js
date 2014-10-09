98075cd337e5e202a761049428d8d2532a9fcea6
Sarah Shank <Sarahshank07@gmail.com>
2014-10-09 12:52:12 -0400

    Expressions_industry
    set up, homework

--- Expressiona_Personal/JS/script.js
+++ Expressiona_Personal/JS/script.js
@@ -13,9 +13,9 @@ var roomWidth = prompt ("Please enter the width of your room in ft.");
 var roomLength = prompt ("Please enter the length of your room ft.");
 //Asking the user for the size of your tile in sq. ft. (Example = 1.77)
 var tileSize = prompt ("Please enter the size of the tile in sq. ft.");
-//Calculate the information
+//Calculate the information.
 var tilesNeeded = roomWidth * roomLength / tileSize;
-//console.log (tilesNeeded); prints to the console
+//console.log (tilesNeeded); prints to the console.
 console.log("Approximately " + tilesNeeded + " tiles are needed.");
 //Alerts the user with the (result) of approximate tiles needed.
 var result = "The total tiles needed for your room is approximately  " + tilesNeeded;
---
+++ Expressions_Industy-Specific/JS/script.js
@@ -0,0 +1,3 @@
+/**
+ * Created by SarahShank on 10/9/14.
+ */
