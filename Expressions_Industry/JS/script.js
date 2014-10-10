3c9a77f9ddaffb1244a4b29f2f866b32b5183f19
Sarah Shank <Sarahshank07@gmail.com>
2014-10-09 20:17:37 -0400

    expressions personal
    JS expressions personal homework

--- Expressions_Industry/JS/script.js
+++ Expressions_Industry/JS/script.js
@@ -6,20 +6,34 @@
  */
 
 //Downloading files
+//Asking the user to enter file size.
 var fileSize = prompt("Please enter your file size");
+//Asking the user to enter internet speed.
 var internetSpeed = prompt("Please enter your internet speed");
-var
+var batteryCharge = prompt("Please enter your battery percentage");
+
+
+var downLoadSpeed = fileSize / internetSpeed;
+var downLoadSpeed = downLoadSpeed + +batteryCharge;
+console.log(downLoadSpeed + " Approximate download speed.");
+
+var example = "Increment Examples Below";
+console.log(example);
 
 var size1 = "File Size ";
 var size2 = "160 bytes";
 
-var result = size1 + size2;
+var result = size1 + size2
 console.log(result);
 
-var  speed1 = "Internet Speed "
-var  speed2 = "10 MB"
+var  speed1 = "Internet Speed ";
+var  speed2 = "10 MB";
 
-var result = speed1 + speed2;
+var result = speed1 + speed2
 console.log(result);
 
-var result =
+var charge1 = "Battery Charge ";
+var charge2 = "45%";
+
+var result = charge1 + charge2
+console.log(result);
