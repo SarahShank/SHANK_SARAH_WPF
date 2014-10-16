84922146da8669561b0b2b36303cdb468f7be8a7
Sarah Shank <Sarahshank07@gmail.com>
2014-10-15 22:09:59 -0400

    Conditional_Personal
    Conditional_Personal, Homework changes

---
+++ Conditional_Industry/JS/script.js
@@ -0,0 +1,17 @@
+/**
+ * Created by SarahShank on 10/15/14.
+ */
+/*
+ Sarah Shank
+ WPF WDD144-O Section 01
+Conditional_Assignment_Industry Week 3
+ 10-15-2014
+ */
+
+
+//created an array
+var dinnerChoices = ["Mexican", "Italian", "Oriental", "Indian"];
+//console.log array
+console.log(dinnerChoices);
+//check how many items are in the array
+console.log(dinnerChoices.length);
