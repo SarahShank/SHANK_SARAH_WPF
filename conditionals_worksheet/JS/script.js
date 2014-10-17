33dbee4b5736f7885d2632b5ecbb2a5741cb8915
Sarah Shank <Sarahshank07@gmail.com>
2014-10-16 22:42:38 -0400

    conditional worksheet
    conditional worksheet

---
+++ conditionals_worksheet/JS/script.js
@@ -0,0 +1,56 @@
+/**
+ * Created by SarahShank on 10/16/14.
+ */
+
+
+
+
+
+//Hot Enough?<br> //Is it hot enough to go to the beach?<br> var temp= 80;<br> //if the temperature is less than 75 to 75<br>      console.log(“We will go to the movies.”);<br> }
+
+var name = prompt("please enter your name");
+if(name === "")
+{
+    name = prompt("Okay Mr. Smith")
+}
+console.log(name)
+
+
+
+var temp = prompt("please enter your current air temperature in degrees celsius or fahrenheit");
+
+if (temp === "celsius" );
+{
+    temp = temp + 32;
+    temp = temp * (9 / 5);
+
+}
+
+    prompt (name + "then it must be" + temp + "degrees fahrenheit");
+
+
+else{
+
+    (temp === "fahrenheit");
+
+
+    temp = temp - 32;
+    temp = temp * (5 / 9);
+
+    prompt("then it must be" + temp + "degrees celsius");
+
+}
+
+if (temp >= 80 + "fahrenheit" || temp >= 26 + "celsius");{
+
+var beach=prompt ("Yay its warm enough to go to the beach!" );
+
+else
+        (temp <= 79 + "fahrenheit" || temp <= 25 + "celsius" );
+
+
+        }
+
+var beach=prompt ("It is too cold to go to the beach!");
+
+
